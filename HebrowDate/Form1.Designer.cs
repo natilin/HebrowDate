481f4da3abdccbfe35d8885e8ba6a3d3187b1eed
Nati Lintzer <nlinzer@gmail.com>
2024-07-24 11:48:13 +0300

    Add project files.

---
+++ HebrowDate/Form1.Designer.cs
@@ -0,0 +1,164 @@
+﻿namespace HebrowDate
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            comboBox_days = new ComboBox();
+            comboBox_month_days = new ComboBox();
+            comboBox_month = new ComboBox();
+            comboBox_year = new ComboBox();
+            button1 = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(335, 32);
+            label1.Name = "label1";
+            label1.Size = new Size(117, 25);
+            label1.TabIndex = 0;
+            label1.Text = "כתיבת תאריך";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(670, 131);
+            label2.Name = "label2";
+            label2.Size = new Size(106, 25);
+            label2.TabIndex = 1;
+            label2.Text = " היום בשבוע";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(477, 131);
+            label3.Name = "label3";
+            label3.Size = new Size(101, 25);
+            label3.TabIndex = 2;
+            label3.Text = "היום בחודש";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(278, 131);
+            label4.Name = "label4";
+            label4.Size = new Size(52, 25);
+            label4.TabIndex = 3;
+            label4.Text = "חודש";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(125, 131);
+            label5.Name = "label5";
+            label5.Size = new Size(45, 25);
+            label5.TabIndex = 4;
+            label5.Text = "שנה";
+            // 
+            // comboBox_days
+            // 
+            comboBox_days.FormattingEnabled = true;
+            comboBox_days.Location = new Point(670, 189);
+            comboBox_days.Name = "comboBox_days";
+            comboBox_days.Size = new Size(118, 33);
+            comboBox_days.TabIndex = 5;
+            // 
+            // comboBox_month_days
+            // 
+            comboBox_month_days.FormattingEnabled = true;
+            comboBox_month_days.Location = new Point(460, 189);
+            comboBox_month_days.Name = "comboBox_month_days";
+            comboBox_month_days.Size = new Size(118, 33);
+            comboBox_month_days.TabIndex = 6;
+            // 
+            // comboBox_month
+            // 
+            comboBox_month.FormattingEnabled = true;
+            comboBox_month.Location = new Point(249, 189);
+            comboBox_month.Name = "comboBox_month";
+            comboBox_month.Size = new Size(118, 33);
+            comboBox_month.TabIndex = 7;
+            // 
+            // comboBox_year
+            // 
+            comboBox_year.FormattingEnabled = true;
+            comboBox_year.Location = new Point(83, 189);
+            comboBox_year.Name = "comboBox_year";
+            comboBox_year.Size = new Size(118, 33);
+            comboBox_year.TabIndex = 8;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(335, 327);
+            button1.Name = "button1";
+            button1.Size = new Size(112, 34);
+            button1.TabIndex = 9;
+            button1.Text = "שלח";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click_1;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(button1);
+            Controls.Add(comboBox_year);
+            Controls.Add(comboBox_month);
+            Controls.Add(comboBox_month_days);
+            Controls.Add(comboBox_days);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private ComboBox comboBox_days;
+        private ComboBox comboBox_month_days;
+        private ComboBox comboBox_month;
+        private ComboBox comboBox_year;
+        private Button button1;
+    }
+}
---
+++ HebrowDate/Form1.cs
@@ -0,0 +1,149 @@
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+namespace HebrowDate
+{
+    public partial class Form1 : Form
+    {
+        public Dictionary<int, string> Days = new Dictionary<int, string>
+        {
+            {1, "יום אחד"},
+            {2, "שני ימים"},
+            {3, "שלשה ימים"},
+            {4, "ארבעה ימים"},
+            {5, "חמישה ימים"},
+            {6, "שישה ימים"},
+            {7, "שבעה ימים"},
+            {8, "שמונה ימים"},
+            {9, "תשעה ימים"},
+            {10, "עשרה ימים"},
+            {11, "אחד עשר יום"},
+            {12, "שנים עשר יום"},
+            {13, "שלשה עשר יום"},
+            {14, "ארבעה עשר יום"},
+            {15, "חמישה עשר יום"},
+            {16, "ששה עשר יום"},
+            {17, "שבעה עשר יום"},
+            {18, "שמונה עשר יום"},
+            {19, "תשעה עשר יום"},
+            {20, "עשרים יום"},
+            {21, "אחד ועשרים יום"},
+            {22, "שניים ועשרים יום"},
+            {23, "שלושה ועשרים יום"},
+            {24, "ארבעה ועשרים יום"},
+            {25, "חמישה ועשרים יום"},
+            {26, "ששה ועשרים יום"},
+            {27, "שבעה ועשרים יום"},
+            {28, "שמונה ועשרים יום"},
+            {29, "תשעה ועשרים יום"},
+            {30, "יום שלשים"}
+        };
+        public Dictionary<string, string> Years = new Dictionary<string, string>
+        {
+            {"תשפ\"ד", "חמשת אלפים ושבע מאות ושמנים וארבע"},
+            {"תשפ\"ה", "חמשת אלפים ושבע מאות ושמנים וחמש"},
+            {"תשפ\"ו", "חמשת אלפים ושבע מאות ושמנים ושש"},
+            {"תשפ\"ז", "חמשת אלפים ושבע מאות ושמנים ושבע"},
+            {"תשפ\"ח", "חמשת אלפים ושבע מאות ושמנים ושמונה"},
+            {"תשפ\"ט", "חמשת אלפים ושבע מאות ושמנים ותשע"},
+            {"תש\"צ", "חמשת אלפים ותשעים"},
+            {"תשצ\"א", "חמשת אלפים ותשעים ואחת"},
+            {"תשצ\"ב", "חמשת אלפים ותשעים ושתיים"},
+            {"תשצ\"ג", "חמשת אלפים ותשעים ושלוש"}
+        };
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            IntializeComboBoxes();
+        }
+        public void IntializeComboBoxes()
+        {
+            //add days of the week
+            comboBox_days.Items.Add("ראשון");
+            comboBox_days.Items.Add("שני");
+            comboBox_days.Items.Add("שלישי");
+            comboBox_days.Items.Add("רביעי");
+            comboBox_days.Items.Add("חמישי");
+            comboBox_days.Items.Add("שישי");
+
+            //add days of the month
+            for (int i = 1; i <= 30; i++)
+            {
+                comboBox_month_days.Items.Add(i);
+            }
+
+            comboBox_month.Items.Add("תשרי");
+            comboBox_month.Items.Add("מרחשון");
+            comboBox_month.Items.Add("כסלו");
+            comboBox_month.Items.Add("טבת");
+            comboBox_month.Items.Add("שבט");
+            comboBox_month.Items.Add("אדר");
+            comboBox_month.Items.Add("אדר הראשון");
+            comboBox_month.Items.Add("אדר השני");
+            comboBox_month.Items.Add("ניסן");
+            comboBox_month.Items.Add("אייר");
+            comboBox_month.Items.Add("סיון");
+            comboBox_month.Items.Add("תמוז");
+            comboBox_month.Items.Add("אב");
+            comboBox_month.Items.Add("אלול");
+
+
+            //add years
+            List<string> years = new List<string>(Years.Keys);
+            foreach (string year in years)
+            {
+                comboBox_year.Items.Add(year);
+            }
+
+
+        }
+        public string CreateQuery()
+        {
+            string dayOfWeek = comboBox_days.Text;
+            string monthDay = Days[Convert.ToInt16(comboBox_month_days.Text)];
+            string month = comboBox_month.Text;
+            string year = Years[comboBox_year.Text];
+            string fullQuery;
+
+            if (dayOfWeek == "ראשון")
+            {
+                dayOfWeek = "אחד";
+            }
+
+
+            if (monthDay == "יום שלשים")
+            {
+                List<string> roshChodesh = new List<string> { "תשרי", "מרחשון", "כסלו", "טבת", "שבט", "אדר", "ניסן", "אייר", "סיון", "תמוז", "אב", "אלול" };
+                fullQuery = $"ביום שלשים בשבת {monthDay} לחודש {month} שהוא ראש חודש{year} לבריאת העולם";
+            }
+            else
+            {
+                fullQuery = $"ב{dayOfWeek} בשבת {monthDay} לירח {month} שנת {year} לבריאת העולם";
+            }
+            return fullQuery;
+        }
+        public void writeTofile(string day, string monthDay, string month, string year, string query)
+        {
+            string path = "C:\\Users\\Owner\\source\\repos\\HebrowDate\\HebrowDate\\data.xml";
+
+            XElement element = new XElement("Query", new XElement("Day", day), new XElement("DayMonth", monthDay),
+                new XElement("Month", month), new XElement("Year", year), new XElement("Result", query)
+                );
+
+            XDocument doc = XDocument.Load(path);
+            doc.Root.Add(element);
+            doc.Save(path);
+        }
+
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            string query = CreateQuery();
+            writeTofile(comboBox_days.Text, comboBox_month_days.Text, comboBox_month.Text, comboBox_year.Text, query);
+            MessageBox.Show("הפרטים נשמרו בהצלחה");
+
+        }
+    }
+}
